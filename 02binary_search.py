44e7662c67262c3a20fc8bdfb4f2cf4660fc15e0
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-14 10:53:32 +0530

    DSA

--- 02binary_search.py
+++ 02binary_search.py
@@ -1,1 +1,15 @@
-def binary_search():
+def binary_search(list, target):
+    first = 0
+    last = len(list)-1 #to find the last value index
+
+    while first <= last:
+        midpoint = (first + last)//2
+
+        if list(midpoint) == target:
+            return midpoint
+        elif list(midpoint) < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+
+
