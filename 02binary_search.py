f0649e4285514df961ce701346bececd74855c41
rajnishspandey <pandeyrajnish86@yahoo.com>
2023-11-14 16:06:03 +0530

    DSA

--- 02binary_search.py
+++ 02binary_search.py
@@ -5,11 +5,23 @@ def binary_search(list, target):
     while first <= last:
         midpoint = (first + last)//2
 
-        if list(midpoint) == target:
+        if list[midpoint] == target:
             return midpoint
-        elif list(midpoint) < target:
+        elif list[midpoint] < target:
             first = midpoint + 1
         else:
             last = midpoint - 1
+    return None
 
+def validate(index):
+    if index is not None:
+        print(f"Target found at the index : {index}")
+    else:
+        print("target not found in the list")
 
+numbers = [1,2,3,4,5,6,7,8,9,10]
+result = binary_search(numbers,12)
+validate(result)
+
+result = binary_search(numbers,8)
+validate(result)
